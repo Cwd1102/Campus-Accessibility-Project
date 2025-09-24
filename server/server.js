e2418d291619fbd9e470abfab766a19e26f59825
cwd1104 <odai1@umbc.edu>
2025-09-23 23:18:49 -0400

    Backend push

--- server/server.js
+++ server/server.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const app = express();
+
+app.get("/api", (req,res) => {
+
+    res.json({ fruits: ["apple", "banana", "john"]})
+});
+
+app.listen(8080,() => {
+    console.log("api listening on 8080")
+});
