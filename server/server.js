0955bd0ff834864534b963d0bcbb96d9ab0c9a35
cwd1104 <odai1@umbc.edu>
2025-09-26 16:03:33 -0400

    edits to backend

--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const app = express();
 var neo4j = require('neo4j-driver');
 
+
 (async () => {
-  // URI examples: 'neo4j://localhost', 'neo4j+s://xxx.databases.neo4j.io'
+
   const URI = process.env.NEO4J_URI
   const USER = process.env.NEO4J_USERNAME
   const PASSWORD = process.env.NEO4J_PASSWORD
@@ -13,17 +14,29 @@ var neo4j = require('neo4j-driver');
   console.log('Connection established')
   console.log(serverInfo)
 
-  // Use the driver to run queries
+
+  let { records, summary } = await driver.executeQuery(`
+      CREATE (a:Person {name: $name})
+      CREATE (b:Person {name: $friendName})
+      CREATE (a)-[:KNOWS]->(b)
+      `,
+    { name: 'Alice', friendName: 'David' },
+    { database: 'neo4j' }
+  )
+  console.log(
+    `Created ${summary.counters.updates().nodesCreated} nodes ` +
+    `in ${summary.resultAvailableAfter} ms.`
+  )
 
   await driver.close()
-})();
 
 
-app.get("/api", (req,res) => {
-
-    res.json({ fruits: ["apple", "banana", "john"]})
 });
 
-app.listen(8080,() => {
-    console.log("api listening on 8080")
+app.get("/api", (req, res) => {
+
+});
+
+app.listen(8080, () => {
+  console.log("api listening on 8080")
 });
