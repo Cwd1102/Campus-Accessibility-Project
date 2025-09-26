dd71c448872dc996bc35e4a1d3002063a84afcdc
cwd1104 <odai1@umbc.edu>
2025-09-25 22:07:11 -0400

    Connected neo4j to backend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,23 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
+var neo4j = require('neo4j-driver');
+
+(async () => {
+  // URI examples: 'neo4j://localhost', 'neo4j+s://xxx.databases.neo4j.io'
+  const URI = process.env.NEO4J_URI
+  const USER = process.env.NEO4J_USERNAME
+  const PASSWORD = process.env.NEO4J_PASSWORD
+  let driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD))
+  const serverInfo = await driver.getServerInfo()
+  console.log('Connection established')
+  console.log(serverInfo)
+
+  // Use the driver to run queries
+
+  await driver.close()
+})();
+
 
 app.get("/api", (req,res) => {
 
