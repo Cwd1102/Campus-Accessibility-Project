5a40ff5ad48eed8c1cc3050b6437c3d45e37c9e8
Ouwen Dai <odai1@umbc.edu>
2025-10-21 23:34:47 -0400

    Added delete and find feature to mongodb

--- myproject/src/components/ReportObstruction.tsx
+++ myproject/src/components/ReportObstruction.tsx
@@ -149,7 +149,7 @@ export default function ReportObstruction() {
     };
 
     try {
-      const res = await fetch("http://localhost:8080/report", {
+      const res = await fetch("http://localhost:8080/report/create", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(entry),
