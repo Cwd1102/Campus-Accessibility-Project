cbd736c9a682a999cdd79ca7bdbafdf22d93f760
dvh3419 <ry47172@umbc.edu>
2025-10-27 14:01:24 -0400

    sarahs pages

--- myproject/src/components/Homepage.tsx
+++ myproject/src/components/Homepage.tsx
@@ -1,44 +1,56 @@
-import { Container} from "react-bootstrap";
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import { Container, Row, Col } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import MapDisplay from "./MapDisplay";
 
 export default function Homepage() {
-  return <span>
-    <>
-    <Container fluid className="p-4 d-flex flex-column">
-        <DropdownButton id="from-button" title="Select your Starting Point">
-          <Dropdown.Item href="#/action-1">RAC</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Sherman Hall</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Biology</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">ILSB</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Commons</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">University Center</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Sondheim Hall</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Math & Psychology</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Performing Arts and Humanities</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Physics</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Public Policy</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">AOK Library & Gallery</Dropdown.Item>
-        </DropdownButton>
-    </Container>
+  return (
+    <Container fluid className="p-4">
+      <Row>
+        {/* Left side - Dropdowns */}
+        <Col md={6} className="d-flex flex-column gap-4">
+          <DropdownButton id="from-button" title="Select your Starting Point">
+            <Dropdown.Item>RAC</Dropdown.Item>
+            <Dropdown.Item>Sherman Hall</Dropdown.Item>
+            <Dropdown.Item>Biology</Dropdown.Item>
+            <Dropdown.Item>ILSB</Dropdown.Item>
+            <Dropdown.Item>Commons</Dropdown.Item>
+            <Dropdown.Item>University Center</Dropdown.Item>
+            <Dropdown.Item>Sondheim Hall</Dropdown.Item>
+            <Dropdown.Item>Math & Psychology</Dropdown.Item>
+            <Dropdown.Item>Performing Arts and Humanities</Dropdown.Item>
+            <Dropdown.Item>Physics</Dropdown.Item>
+            <Dropdown.Item>Public Policy</Dropdown.Item>
+            <Dropdown.Item>AOK Library & Gallery</Dropdown.Item>
+          </DropdownButton>
 
-    <Container fluid className="p-4 d-flex flex-column">
-      <DropdownButton id="to-button" title="Select your Destination">
-        <Dropdown.Item href="#/action-1">RAC</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Sherman Hall</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Biology</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">ILSB</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Commons</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">University Center</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Sondheim Hall</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Math & Psychology</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Performing Arts and Humanities</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Physics</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Public Policy</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">AOK Library & Gallery</Dropdown.Item>
-      </DropdownButton>
+          <DropdownButton id="to-button" title="Select your Destination">
+            <Dropdown.Item>RAC</Dropdown.Item>
+            <Dropdown.Item>Sherman Hall</Dropdown.Item>
+            <Dropdown.Item>Biology</Dropdown.Item>
+            <Dropdown.Item>ILSB</Dropdown.Item>
+            <Dropdown.Item>Commons</Dropdown.Item>
+            <Dropdown.Item>University Center</Dropdown.Item>
+            <Dropdown.Item>Sondheim Hall</Dropdown.Item>
+            <Dropdown.Item>Math & Psychology</Dropdown.Item>
+            <Dropdown.Item>Performing Arts and Humanities</Dropdown.Item>
+            <Dropdown.Item>Physics</Dropdown.Item>
+            <Dropdown.Item>Public Policy</Dropdown.Item>
+            <Dropdown.Item>AOK Library & Gallery</Dropdown.Item>
+          </DropdownButton>
+        </Col>
+
+        {/* Right side - MapDisplay with title */}
+        <Col
+          md={6}
+          className="d-flex flex-column align-items-center justify-content-center"
+        >
+          <h4 className="mb-3 text-center">Campus Map</h4>
+          <MapDisplay />
+        </Col>
+      </Row>
     </Container>
-  </>
-  </span>;
+  );
 }
+
 
